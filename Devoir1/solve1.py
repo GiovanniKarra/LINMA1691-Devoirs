552daf6dda85a7e3251eae3d75fdaceb3686f048
Giovanni Karra <giovanni.karra@student.uclouvain.be>
2023-10-17 11:23:30 +0200

    YESSSSSSSSSSSSSSS

--- Devoir1/solve1.py
+++ Devoir1/solve1.py
@@ -1,5 +1,13 @@
 from collections import deque
 
+def dfs(x, visited, g):
+    q = [x]
+    while q:
+        x = q.pop()
+        visited[x] = True
+        for adj in g[x]:
+            if visited[adj]: continue
+            q.append(adj)
 
 """
     Solves the problem defined in the statement for adj an adjacency list of the dispersion dynamics of rumors in LLN
@@ -8,7 +16,7 @@ from collections import deque
 
     You are free to change the code below and to not use the precompleted part. The code is based on the high-level description at https://en.wikipedia.org/wiki/Kosaraju%27s_algorithm
     You can also define other sub-functions or import other datastructures from the collections library
-"""
+"""   
 def solve(adj):
     # adjacency of the graph and its transpose
     adj_out = adj
@@ -17,68 +25,55 @@ def solve(adj):
     # number of nodes
     N = len(adj_in)
 
-    # is a node already visited?
-    visited = [False]*N
-    # list of node to process in the second step
     L = []
+    
     # queue of nodes to process with their associated status (i,False/True) i is the node index and True/False describes if we are appending the node to L or not when processing it
-    q = deque()
-
+    q = []
+    visited = [False]*N
     ### loop on every node and launch a visit of its descendants
-
     for x in range(N):
-        if not visited[x]:
-            q.append((x, False))
+        if visited[x]:
+            continue
+        q.append((x,False))
 
-            while q:
-                u, to_add = q.pop()
-
-                if to_add:
-                    L.append(u)
-                    continue
-                visited[u] = True
-                q.append((u, True))
-                out_neighbours = adj_out[u]
-                for v in out_neighbours:
-                    if not visited[v]:
-                        q.append((v, False))
+        while q:
+            x, to_add = q.pop()
+            
+            if to_add:
+                L.append(x)
+                continue
+            visited[x] = True
+            q.append((x,True))
+            for y in adj[x]:
+                if not visited[y]:
+                    q.append((y,False))
 
     ### reverse the list to obtain the post-order
     L.reverse()
-
+    # print(L)
     ### find the strongly connected components
     ans = 0
+
     visited = [False]*N
     for x in L:
-        if not visited[x]:
-            ans += 1
-            q = deque()
-            q.append(x)
-            while q:
-                x = q.pop()
-                visited[x] = True
-                out_neighbours = adj_out[x]
-                for v in out_neighbours:
-                    if not visited[v]:
-                        q.append(v)
-
-    
+        if visited[x]:
+            continue
+        ans += 1
+        dfs(x, visited, adj_out)
     return ans
+
 
 """
     Transpose the adjacency matrix
         Construct a new adjacency matrix by inverting all the edges: (x->y) becomes (y->x) 
 """
 def transpose(adj):
-    n = len(adj)
-    adj_in = [[] for _ in range(n)]
-    
-    for i in range(n):
-        for j in adj[i]:
-            adj_in[j].append(i)
+    adj_in = [list() for _ in range(len(adj))]
 
+    for i in range(len(adj)):
+        for x in adj[i]:
+            adj_in[x].append(i)
     return adj_in
-
 
 if __name__ == "__main__":
     matrix = [[1],
