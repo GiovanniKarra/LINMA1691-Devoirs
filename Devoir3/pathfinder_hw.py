0123d0824857acc1ff12d12c6906897e3b79aee9
Giovanni Karra <karragiovanni@gmail.com>
2023-11-21 12:59:51 +0100

    devoir 3 fait

--- Devoir3/pathfinder_hw.py
+++ Devoir3/pathfinder_hw.py
@@ -6,22 +6,36 @@ Si aucun chemin eulérien n'existe, la fonction retourne None.
 L'argument graph ne doit pas être modifié lors de l'exécution de la fonction.
 """
 def eulerian_path_finder(graph):
-    """https://www.geeksforgeeks.org/hierholzers-algorithm-directed-graph/"""
     adj = copy.deepcopy(graph)
     N = len(adj)
+
+    odd_degree = 0
+    for a in adj:
+        odd_degree += len(a)%2
+
+    if odd_degree not in (0, 2):
+        return None
+
     eulerian_path = []
     stack = []
+
+    visited_edges = set()
 
     stack.append(0)
 
     while stack:
         current_node = stack[-1]
         if len(adj[current_node]) > 0:
-            stack.append(adj[current_node][-1])
-            adj[current_node].pop()
-        else:
-            eulerian_path.append(current_node)
-            stack.pop()
-            
+            while len(adj[current_node]) != 0 and\
+                    ((current_node, adj[current_node][-1]) in visited_edges\
+                     or (adj[current_node][-1], current_node) in visited_edges):
+                adj[current_node].pop()
+            if len(adj[current_node]) > 0:
+                stack.append(adj[current_node][-1])
+                adj[current_node].pop()
+                visited_edges.add((current_node, stack[-1]))
+                continue
+        eulerian_path.append(current_node)
+        stack.pop()
 
     return eulerian_path
