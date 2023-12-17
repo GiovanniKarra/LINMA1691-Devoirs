7ece9f7441b43ee9f28f97f2db33682004db24a8
Giovanni Karra <karragiovanni@gmail.com>
2023-12-17 12:54:18 +0100

    de la merde

--- Devoir4/template.py
+++ Devoir4/template.py
@@ -12,9 +12,6 @@ class Edge:
 
 
 def create_graph(capacities, costs, green_sources: dict, gas_centrals: dict, consumers: dict):
-
-    # TODO
-
     s = 0
     t = 0
     graph = []
@@ -22,14 +19,23 @@ def create_graph(capacities, costs, green_sources: dict, gas_centrals: dict, con
 
 
 def get_residual(graph):
+    N = len(graph)
+    graph_residual = [[] for _ in range(N)]
 
-    # TODO
+    for u in range(N):
+        for edge in graph[u]:
+            graph_residual[u].append(Edge(edge.u, edge.v, edge.capa, edge.weight))
+            graph_residual[edge.v].append(Edge(edge.v, edge.u, 0, -edge.weight))
+            graph_residual[u][-1].residual = graph_residual[edge.v][-1]
 
-    graph_residual = graph
     return graph_residual
 
 
-def get_path(t, parents: list[Edge]):
+def get_path(s, t, parents: list[Edge]):
+    tmp = parents.copy()
+    tmp[s] = 0
+    if None in tmp:
+        return False, INF
     path = []
     mincap = INF
     current = parents[t]
@@ -39,12 +45,17 @@ def get_path(t, parents: list[Edge]):
         path.append(current)
         current = parents[current.u]
 
+    path.reverse()
     return path, mincap
 
 
-def min_cost_max_flow(s, t, graph_residual: list[Edge]):
+def min_cost_max_flow(s, t, graph_residual: list[list[Edge]]):
 
     N = len(graph_residual)
+
+    # for l in graph_residual:
+    #     for edge in l:
+    #         edge.residual = Edge(edge.u, edge.v, edge.capa, edge.weight)
 
     def BellmanFord():
         parents = [None]*N
@@ -52,9 +63,11 @@ def min_cost_max_flow(s, t, graph_residual: list[Edge]):
         d[s] = 0
         inque = set()
         queue = deque()
+        queue.append(s)
+        inque.add(s)
         while queue:
             u = queue.popleft()
-            if u in inque: inque.remove(u)
+            inque.remove(u)
             for edge in graph_residual[u]:
                 if edge.weight + d[u] < d[edge.v]:
                     d[edge.v] = edge.weight + d[u]
@@ -62,21 +75,19 @@ def min_cost_max_flow(s, t, graph_residual: list[Edge]):
                     if edge.v not in inque:
                         inque.add(edge.v)
                         queue.append(edge.v)
-        return d, parents
-
-    flow = [[0]*N for _ in range(N)]
-    d, parents = BellmanFord()
-    path, mincap = get_path(t, parents)
-    if mincap >= 0:
-        for edge in path:
-            flow[edge.u][edge.v] += mincap
-            flow[edge.v][edge.u] -= mincap
+        return get_path(s, t, parents)
 
     maximum_flow = 0
-    for i in range(N):
-        for j in range(N):
-            if f := flow[i][j] > maximum_flow:
-                maximum_flow(f)
-    minimum_cost = sum([edge.weight for edge in parents if edge != None])
+    minimum_cost = 0
 
+    path, mincap = BellmanFord()
+    while path and mincap > 0:
+        maximum_flow += mincap
+        for edge in path:
+            minimum_cost += mincap*edge.weight
+            edge.capa -= mincap
+            edge.residual.capa += mincap
+        path, mincap = BellmanFord()
+
+    print(maximum_flow)
     return maximum_flow, minimum_cost
